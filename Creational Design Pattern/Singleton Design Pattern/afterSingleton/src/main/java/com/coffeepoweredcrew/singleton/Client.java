2df4c91f074a0e0e9dc08f0208b0f7b9e6a3aaaf
Bappaditya Dutta <bappadityadutta2019@gmail.com>
2022-02-22 07:12:51 +0530

    Lazy Singleton using Double check locking

--- Creational Design Pattern/Singleton Design Pattern/afterSingleton/src/main/java/com/coffeepoweredcrew/singleton/Client.java
+++ Creational Design Pattern/Singleton Design Pattern/afterSingleton/src/main/java/com/coffeepoweredcrew/singleton/Client.java
@@ -9,6 +9,14 @@ public class Client {
 		singleton = EagerRegistry.getInstance();
 		EagerRegistry singleton2 = EagerRegistry.getInstance();
 		System.out.println(singleton == singleton2);
+		
+		LazyRegistryWithDCL lazysingleton1=LazyRegistryWithDCL.getInstance();
+		
+		LazyRegistryWithDCL lazysingleton2=LazyRegistryWithDCL.getInstance();
+		
+		System.out.println(lazysingleton1 == lazysingleton2);
+		
+		
 	}
 
 }
